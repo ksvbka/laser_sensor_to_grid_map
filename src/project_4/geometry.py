3dc554f2b1ef472106a46b33229fcec9a8b1972f
Trung Kien <letrungkien.k53.hut@gmail.com>
2021-08-09 22:19:40 +0700

    implement geometry

--- src/project_4/geometry.py
+++ src/project_4/geometry.py
@@ -4,21 +4,30 @@ import math
 # Convert world coordinate to grid coordinate
 # ------------------------------------------------------------------------------
 def to_grid(x, y, origin_x, origin_y, size_x, size_y, resolution):
+  if (x < origin_x or x >= resolution * size_x) or \
+     (y < origin_y or y >= resolution * size_y):
+    return None
 
-  return None
+  gx = int((x - origin_x) / resolution)
+  gy = int((y - origin_y) / resolution)
+  return (gx, gy)
     
 # ------------------------------------------------------------------------------
 # Convert grid coordinate to world coordinate
 # ------------------------------------------------------------------------------
 def to_world(gx, gy, origin_x, origin_y, size_x, size_y, resolution):
-
-  return None
+  if (gx < 0 or gx > size_x) or (gy < 0 or gy > size_y):
+    return None
+  
+  x = resolution * (gx + 0.5) + origin_x
+  y = resolution * (gy + 0.5) + origin_y
+  return (x, y)
   
 # ------------------------------------------------------------------------------
 # Convert grid coordinate to map index
 # ------------------------------------------------------------------------------
 def to_index(gx, gy, size_x):
-  return gy * size_x + gx
+  return int(gy * size_x + gx)
 
 # ------------------------------------------------------------------------------
 # Given two integer coordinates return a list of coordinates of a line between 
@@ -56,7 +65,7 @@ def bresenham(x0, y0, x1, y1):
     # Iterate over bounding box generating points between start and end
     y = y0
     points = []
-    for x in range(x0, x1 + 1):
+    for x in range(int(x0), int(x1 + 1)):
         coord = (y, x) if is_steep else (x, y)
         points.append(coord)
         error -= abs(dy)
